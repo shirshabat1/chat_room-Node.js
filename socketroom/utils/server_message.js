f0ccd8b83601afae7a56eb5d7c8ab4e1290b82e8
Shir Shabat <77749228+shirshabat1@users.noreply.github.com>
2023-07-09 14:40:31 +0300

    Delete socketroom directory

--- socketroom/utils/server_message.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-function serverMessage(randomColor,username,  message)
-{
-    // const newmessage= {time: `${moment().format('h:mm a')}` ,message: `${message}` }
-    return {randomColor, username, message, time: `${moment().format('MMMM Do YYYY, h:mm:ss a')}`}
-}
-
-
-module.exports  ={serverMessage};
